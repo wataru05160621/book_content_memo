8b1fdac35375ad4faf2a33d2dbfd011170038004
wataru05160621 <wataru.s.05160621@gmail.com>
2025-05-17 10:24:04 +0900

    デプロイチェック

--- frontend/src/components/DebugLogger.tsx
+++ frontend/src/components/DebugLogger.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, ScrollView, StyleSheet } from 'react-native';
+import { Text, ScrollView, StyleSheet } from 'react-native';
 
 interface DebugLoggerProps {
   logs: string[];
